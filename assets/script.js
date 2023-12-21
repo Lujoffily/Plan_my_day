70df2c90dcf45fb3bc694966bf9b756bf60ead8e
Luna Joffily <luna.joffily@gmail.com>
2023-12-20 23:12:24 -0500

    saved text to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,29 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+  // Display current day
+$('#currentDay').text(dayjs().format('MMM D, YYYY'));
+
+// Time-block color-coded
+$('.time-block').each(function () {
+  var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+  var time = dayjs().format('HH');
+  if (timeBlock < time) {
+    $(this).addClass('past');
+  } else if (time === timeBlock) {
+    $(this).removeClass('past');
+    $(this).addClass('present');
+  } else {
+    $(this).removeClass('past');
+    $(this).removeClass('present');
+    $(this).addClass('future');
+  }
+});
+
+var textInp
+var textStorage
+
+// save text into local storage
+$('.saveBtn').on("click", function () {
+  textInp = $(this).siblings(".description").val(); 
+  textStorage = $(this).parent().attr("id");
+  localStorage.setItem(textStorage, textInp);
+});
+
