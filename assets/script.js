107d693b9fb0f758fd55528096a60946d04146b6
Luna Joffily <lunajoffily@Lunas-MacBook-Air.local>
2023-12-22 17:34:20 -0500

    debugging

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,13 @@
 $('#currentDay').text(dayjs().format('MMM D, YYYY'));
 
 // Time-block color-coded
+var colorCode = function() {
 $('.time-block').each(function () {
   var timeBlock = parseInt($(this).attr("id").split("-")[1]);
   var time = dayjs().format('HH');
   if (timeBlock < time) {
     $(this).addClass('past');
-  } else if (time === timeBlock) {
+  } else if (time == timeBlock) {
     $(this).removeClass('past');
     $(this).addClass('present');
   } else {
@@ -15,13 +16,15 @@ $('.time-block').each(function () {
     $(this).removeClass('present');
     $(this).addClass('future');
   }
-  console.log(time);
 });
+}
 
+colorCode();
 var textInp
 var textStorage
 
 // save text into local storage
+
 $('.saveBtn').on("click", function () {
   textInp = $(this).siblings(".description").val(); 
   textStorage = $(this).parent().attr("id");
